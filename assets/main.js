3abecf65a4ecffa70a4d99e7331301fb1f135be0
Giuliano Neves Damasceno <giuliano131@hotmail.com>
2021-05-04 01:11:03 -0300

    Conversor de moedas funcional

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,43 @@
-let teste3, teste2;
-fetch(`http://economia.awesomeapi.com.br/json/last/USD-BRL`, teste())
-    // .then( T => teste =T.json())
-    // .then(console.log(teste))
+"use strict"
+
+const botaoConverter = document.getElementById("btnConverter");
+
+botaoConverter.addEventListener("click", converteMoeda)
+function converteMoeda() {
+    const moedaEntrada = document.getElementById("selectEntrada").value;
+    const moedaSaida = document.getElementById("selectSaida").value;
+    const valor = document.getElementById("valorInserido").value;
+    const resultado = document.getElementById("btnResult");
+    let conversao = 0;
+
+    if (moedaEntrada === moedaSaida) {
+        exibeResultado(moedaSaida, valor)
+    }
+    else {
+        fetch(`http://economia.awesomeapi.com.br/json/last/${moedaEntrada}-${moedaSaida}`)
+            .then(T => T.json())
+            .then(resposta => {
+                for (const cotacoes in resposta) {
+                    if (resposta.hasOwnProperty.call(resposta, cotacoes)) {
+                        const cotacao = resposta[cotacoes];
+                        conversao = (parseFloat(valor) * parseFloat(cotacao.ask)).toFixed(2);
+                        console.log(conversao)
+                        exibeResultado(moedaSaida, conversao);
+                    }
+                }
+            }
+            )
+    }
+}
+
+function exibeResultado(tipoDeMoeda, valor) {
+    const resultado = document.getElementById("btnResult");
+   
+    if (tipoDeMoeda == "BRL") {
+        resultado.innerText = `R$${valor}`;
+    } else if (tipoDeMoeda == "USD") {
+        resultado.innerText = `$${valor}`;
+    } else if (tipoDeMoeda == "EUR")
+        resultado.innerText = `${valor}€`;
+
+}
