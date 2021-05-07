85cbb2404cc284e81b160d21ebdce13e8685a711
Giuliano Neves Damasceno <giuliano131@hotmail.com>
2021-05-07 11:00:43 -0300

    Atualização async/await

--- assets/main.js
+++ assets/main.js
@@ -9,32 +9,50 @@ function converteMoeda() {
     const moedaEntrada = document.getElementById("selectEntrada").value;
     const moedaSaida = document.getElementById("selectSaida").value;
     const valor = document.getElementById("valorInserido").value;
-    
+
     let conversao = 0;
 
     if (moedaEntrada === moedaSaida) {
         exibeResultado(moedaSaida, valor)
     }
     else {
-        fetch(`https://economia.awesomeapi.com.br/json/last/${moedaEntrada}-${moedaSaida}`)
-            .then(T => T.json())
+        const pegaCotacao = async () => {
+            try {
+                const response = await fetch(`https://economia.awesomeapi.com.br/json/last/${moedaEntrada}-${moedaSaida}`)
+                const ListaCotacoes = await response.json()
+
+                for (const cotacoes in ListaCotacoes) {
+                    const cotacao = ListaCotacoes[cotacoes];
+                    conversao = (parseFloat(valor) * parseFloat(cotacao.ask)).toFixed(2);
+                    console.log(cotacao)
+                    exibeResultado(moedaSaida, conversao);
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        pegaCotacao()
+    }
+}
+/*
+fetch(`https://economia.awesomeapi.com.br/json/last/${moedaEntrada}-${moedaSaida}`)
+            .then(T => T.json()
+            )
             .then(resposta => {
                 for (const cotacoes in resposta) {
                     if (resposta.hasOwnProperty.call(resposta, cotacoes)) {
                         const cotacao = resposta[cotacoes];
                         conversao = (parseFloat(valor) * parseFloat(cotacao.ask)).toFixed(2);
-                        console.log(conversao)
+                        console.log(cotacao)
                         exibeResultado(moedaSaida, conversao);
                     }
                 }
             }
-            )
-    }
-}
+*/
 
 function exibeResultado(tipoDeMoeda, valor) {
-    const resultado = document.getElementById("btnResult"); 
-    
+    const resultado = document.getElementById("btnResult");
+
     if (tipoDeMoeda == "BRL") {
         resultado.innerText = parseFloat(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     } else if (tipoDeMoeda == "USD") {
